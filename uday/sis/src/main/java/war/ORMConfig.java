6aec05bc029bcf789a0944f02b7f54ede5a35188
123 <udaykrishna153@gmail.com>
2018-05-18 11:31:59 +0530

    kfjksjf

---
+++ uday/sis/src/main/java/war/ORMConfig.java
@@ -0,0 +1,7 @@
+package war;
+
+import at.chrl.orm.hibernate.configuration.JPAConfig;
+
+public class ORMConfig extends JPAConfig {
+
+}
---
+++ uday/sis/src/main/java/war/SpringConfig.java
@@ -0,0 +1,15 @@
+package war;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import at.chrl.orm.hibernate.configuration.JPAConfig;
+
+@Configuration
+public class SpringConfig {
+
+	@Bean
+	public JPAConfig getJPAConfig(){
+		return new ORMConfig();
+	}
+}
